f8a84916081a0edd2fbe6294b403ebbd5d3ffa5e
yongjun <lyj578520@naver.com>
2026-09-08 19:16:50 +0900

    Revert "20260908 DTO 커밋"
    This reverts commit 3d396a13dbab4ebd94010e90df9369320b000971.

--- DTO/OptionDTO.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class Option(SQLModel, table=True):
-    option_code : int | None = Field(
-        default = None,
-        primary_key = True
-    )
-    option_name : str
