343e29e2637cdbd63e9c3f8dad6cbeafa5a00d89
overwinna <ydy7071@gmail.com>
2026-09-09 14:30:16 +0900

    웰컴 팝업 추가

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,17 +1,37 @@
 
 // 팝업창을 작동로직
+// 웹 페이지가 로딩되면 팝업창의 내부 버튼을 찾아서 이벤트를 발생시킴
 // 무조건 닫히는 경우, 7일간 안보기 클릭시 쿠키가 적용되는 구조
-window.onload=()=>{
+window.onload = () => {
     document.querySelector('.popup .close').addEventListener(
-        'click',function(event){
+        'click', function (event) {
+
             console.log(event.target.parentElement.parentElement)
+
             event.target.parentElement.parentElement.classList.add('hide')
-            if(event.target.parentElement.querySelector('.chk').checked){
-                document.cookie='welcomePopup; max-age=10'
+            if (event.target.parentElement.querySelector('.chk').checked) {
+                document.cookie = `welcomePopup=True;  max-age=${60 * 60 * 24 * 7}; path=/`
             }
+            
         }
-        // 쿠키 먹이는 부분
-    
     )
-//   max-age가 만료일이며, 단위는 초
+    value=getCookieValue('welcomePopup')
+
+    if(value!=null){
+        document.querySelector('.popup').classList.add('hide')
+    }
+   
+}
+
+function getCookieValue(key){
+    cookies=document.cookie.split('; ')
+    for(cookie of cookies)
+        
+        names=cookie.split('=')[0]
+        value=cookie.split('=')[1]
+
+        if(names==key){
+            return value
+        }
+        return null
 }
