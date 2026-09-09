72ccb55bbb50c6f18e292430f4212b127043bece
DESKTOP-5QGIGNM\admin <d16udonne04@gmail.com>
2026-09-09 16:12:56 +0900

    api update

--- api.py
+++ api.py
@@ -34,21 +34,19 @@ app.mount(
 @app.get('/dsinside')
 def main(request:Request):
     return templates.TemplateResponse(request,'main.html')
+
 # 로그인 화면으로 넘어가는 곳 
 @app.get('/login')
 def login(request:Request):
-  return templates.TemplateResponse(request,'login.html')
+    return templates.TemplateResponse(request,'login.html')
 
-# 검색후 넘어가는곳 
-
-
+# 검색 후 넘어가는곳 
 @app.get('/search')
 def search(request:Request):
- return templates.TemplateResponse(request,'search.html')
+    return templates.TemplateResponse(request,'search.html')
 
 
 # 현재는 id랑 pw가 같은지 비교하는 로직이 존재하지 않음
-
 @app.post('/api/login')
 def _login():
     try:
@@ -59,7 +57,8 @@ def _login():
     return RedirectResponse(               
              url='/dsinside',
              status_code=303 # 303: 무조건 GET으로 다시 들어오게 한다
-                   )  
+                   )
+
 # 리뷰 수정을 하러 가는 곳
 # 리뷰 수정을 하고 나서는 리뷰 수정한 내용을 보여주고 다시 원래 대로 돌아가는게 나을거같음
 @app.get('/restaurant/update')   
@@ -69,12 +68,12 @@ def restaurantUpdate(request:Request):
     
 @app.get('/review/list')
 def review(request:Request, 
-           session:Session = Depends(get_session)):
+        session:Session = Depends(get_session)):
     
     try:
         sql = text('''
                    select m.name, r.review_content, r.rating, r.review_time
-                   from review as r join member as m usint(member_code)
+                   from review as r join member as m using(member_code)
                    ''')
         
         result = session.execute(sql)
@@ -88,11 +87,11 @@ def review(request:Request,
     })
 
 @app.get('/review/add')
-def review(request:Request):
+def review_add(request:Request):
     return templates.TemplateResponse(request,'review_add.html')
 
 @app.post('/review/add')
-def review_add(
+def review_add2(
     review: Review =Form(),
     session:Session = Depends(get_session)
     ):
@@ -114,20 +113,20 @@ def review_add(
 # 회원 가입창 넘어가는 부분
 @app.get('/signup')
 def sing_up(request:Request):
- return templates.TemplateResponse(request,'sign_up.html')
+    return templates.TemplateResponse(request,'sign_up.html')
     
 @app.post('/api/signup')
 def _signup():
     
     
- return RedirectResponse(               
-    url='/bap',
-    status_code=303 # 303: 무조건 GET으로 돌아오게 함
-                   )   
+    return RedirectResponse(               
+        url='/bap',
+        status_code=303 # 303: 무조건 GET으로 돌아오게 함
+    )   
     
 @app.get('/mypage')
 def mypage(request:Request):
- return templates.TemplateResponse(request,'mypage.html')    
+    return templates.TemplateResponse(request,'mypage.html')    
 
 
 
@@ -139,7 +138,7 @@ def mypage(request:Request):
 @app.get('/mypage/reviews')
 def reviews(request:Request):
     
- return templates.TemplateResponse(request,'review_list.html')    
+    return templates.TemplateResponse(request,'review_list.html')    
 
 
 
@@ -150,5 +149,5 @@ def reviews(request:Request):
 
 # 서버 키는 곳
 if __name__ == '__main__':
- import uvicorn
- uvicorn.run('api:app', port=8000, reload=True,host='0.0.0.0')
+    import uvicorn
+    uvicorn.run('api:app', port=8000, reload=True,host='0.0.0.0')
